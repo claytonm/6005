908fb7bfdd1bd81b602764cf7e8b6c745f2d23fc
Clay <claytonm@gmail.com>
2015-09-13 14:14:59 -0400

    Solutions to Problem Set 2

--- ps2/src/piano/PianoMachineTest.java
+++ ps2/src/piano/PianoMachineTest.java
@@ -19,17 +19,69 @@ public class PianoMachineTest {
         String expected0 = "on(61,PIANO) wait(100) off(61,PIANO)";
         
     	Midi midi = Midi.getInstance();
-
     	midi.clearHistory();
-    	
+
+    	// Problem 1: test beginNote and endNote
         pm.beginNote(new Pitch(1));
-        pm.beginNote(new Pitch(4));
+        // pm.beginNote(new Pitch(4));
 		Midi.wait(100);
 		pm.endNote(new Pitch(1));
-        pm.endNote(new Pitch(4));
+        // pm.endNote(new Pitch(4));
 
         System.out.println(midi.history());
-        assertEquals(expected0,midi.history());
+        assertEquals(expected0, midi.history());
+
+        // Problem 2: test switchInstruments
+        midi.clearHistory();
+        String expected1 = "on(61,PIANO) wait(100) off(61,PIANO) wait(0) on(61,BRIGHT_PIANO) wait(100) off(61,BRIGHT_PIANO)";
+
+        pm.beginNote(new Pitch(1));
+        // pm.beginNote(new Pitch(4));
+        Midi.wait(100);
+        pm.endNote(new Pitch(1));
+        pm.changeInstrument();
+        pm.beginNote(new Pitch(1));
+        // pm.beginNote(new Pitch(4));
+        Midi.wait(100);
+        pm.endNote(new Pitch(1));
+
+        System.out.println(midi.history());
+        assertEquals(expected1, midi.history());
+
+        // Problem 3: test switchUp and switchDown
+        midi.clearHistory();
+        String expected3 = "on(61,BRIGHT_PIANO) wait(100) off(61,BRIGHT_PIANO) wait(0) on(73,BRIGHT_PIANO) wait(100) off(73,BRIGHT_PIANO) wait(100) on(61,BRIGHT_PIANO) wait(100) off(61,BRIGHT_PIANO)";
+
+        pm.beginNote(new Pitch(1));
+        Midi.wait(100);
+        pm.endNote(new Pitch(1));
+        pm.shiftUp();
+        pm.beginNote(new Pitch(1));
+        Midi.wait(100);
+        pm.endNote(new Pitch(1));
+        pm.shiftDown();
+        Midi.wait(100);
+        pm.beginNote(new Pitch(1));
+        Midi.wait(100);
+        pm.endNote(new Pitch(1));
+
+        System.out.println(midi.history());
+        assertEquals(expected3, midi.history());
+
+        // Problem 4: test switchUp and switchDown
+        midi.clearHistory();
+        String expected4 = "on(61,BRIGHT_PIANO) wait(100) off(61,BRIGHT_PIANO) wait(0) on(61,BRIGHT_PIANO) wait(100) off(61,BRIGHT_PIANO)";
+
+        pm.toggleRecording();
+        pm.beginNote(new Pitch(1));
+        Midi.wait(100);
+        pm.endNote(new Pitch(1));
+        pm.playback();
+
+        System.out.println(midi.history());
+        assertEquals(expected4, midi.history());
+
+
     }
 
 }
